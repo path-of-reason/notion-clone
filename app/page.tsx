e76d4819c2f4570d096cda59c7842889593d106b
wistaria <kogoome@kakao.com>
2023-11-13 10:01:42 +0900

    space test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
-	return (
+	return (  
 		<div className="w-screen h-screen p-10">
 			<h1 className="text-red-400 font-bold text-5xl">Hello Notion Clone</h1>
 			<Button variant="purple" size="default">
